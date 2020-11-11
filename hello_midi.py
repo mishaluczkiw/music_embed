f3037b2259724da4c77e21242af8f729622294d3
misha <mluczkiw@maxentric.com>
2020-11-11 14:41:03 -0500

    First commit of final project. Loading MIDI file. Will try to push to remote

---
+++ hello_midi.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 01:57:33 2020
+Load MIDI file
+@author: Misha Luczkiw & Nick Triano
+"""
+
+import mido as md
+import os.path
+
+
+
+folder = "beeth"
+
+fn = "waldstein_3.mid"
+fn1 = "waldstein_2.mid"
+fn2 = os.path.join(folder,fn1)
+
+print(fn2)
+
+
+#f = open(fn)
+#fn2 = "C:\Users\MaXentric\Desktop\Misha\Deep Learning\final\beeth"
+
+song = md.MidiFile(fn2)
+print(song)
+
+for track in song.tracks:
+    print(track)
+
